131512af94a8691a751fde811560d0ea3aae6cd9
T-Darjus <108716930+T-Darjus@users.noreply.github.com>
2022-09-05 13:11:30 +0300

    Add files via upload

---
+++ src/pages/aboutUs/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "../../components/about/About";
+import Work from "../../components/work/Work";
+import Questions from "../../components/questions/Questions";
+
+const AboutUs = () => {
+  return (
+    <>
+      <About />
+      <Work />
+      <Questions />
+    </>
+  );
+};
+
+export default AboutUs;
